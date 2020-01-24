37fe55ca99343f98d326b04a0ebc94e4731687e9
ChristianMc2 <59711732+ChristianMc2@users.noreply.github.com>
2020-01-24 11:46:24 -0800

    Delete Basic_JavaScript_1.js

--- Basic JavaScript Projects/Basic JavaScript Projects/JS/Basic_JavaScript_1.js
+++
@@ -1,7 +0,0 @@
-var x = "\"How goes it\" Jackie asked " + "  before he left ", y = " &nbsp; &nbsp; Goodbye ";
-var x = x.fontcolor("red");
-var y = y.fontcolor("green");
-
-document.write(x);
-document.write(y);
-document.write(5 * 4);
