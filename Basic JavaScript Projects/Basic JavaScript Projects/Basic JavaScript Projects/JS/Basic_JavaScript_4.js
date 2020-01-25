d728b7a5c11fead1b95660ff3dea9083c1e86782
ChristianMc2 <59711732+ChristianMc2@users.noreply.github.com>
2020-01-25 05:09:25 -0800

    Delete Basic_JavaScript_4.js

--- Basic JavaScript Projects/Basic JavaScript Projects/Basic JavaScript Projects/JS/Basic_JavaScript_4.js
+++
@@ -1,9 +0,0 @@
-function my_Dictionary() {
-    var Shirt = {
-        Size: "Large",
-        Color: "Grey",
-        Material: "Polyester",       
-    };
-    delete Shirt.Size;
-    document.getElementById("Dictionary").innerHTML = Shirt.Size;
-}
